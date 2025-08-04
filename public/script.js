8ee0b8a9d9f85afdae3d80510af811284a5ed7fe
oladiran ayanfe <oladiranayanfe7@gmail.com>
2025-08-04 08:40:37 +0100

    fix: Uncomment welcome message display for authenticated users

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         currentUser = user;
         show.innerHTML = `
-        <!---------------   <h3>Welcome ${user.displayName}</h3> --------------->
+        <h3>Welcome ${user.displayName}</h3>
             <img src="${user.photoURL}" alt="Profile Picture" style="width: 30px; height: 30px; border-radius: 100%;" />
         `;
         loadNotes();
